c1e3e29abf3e78ee5e7bfd9100184686b39614f8
mahesh <maekunde@gmail.com>
2023-07-27 22:51:44 +0530

    added welcome

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('hello World')
+    name= "Welcome"
+    print_hi(f'hello World:{name}')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
